eb8c0c4e0f7be0a8646493dbadb752bc14f80bbd
Steven Draugel <draugelsm@g.cofc.edu>
2016-02-09 10:13:38 -0500

    added an RDY file and updated the RSD class with some sudocode

--- TSD-parser.py
+++ TSD-parser.py
@@ -4,36 +4,48 @@
 import re
 import sys
 
-def parser():
+class TSD:
+    def parser(folderName, fileName, excelFile):
 
 
-    start = 0
-    count = 0
-    total = 1
+        start = 0
+        count = 0
+        total = 1
+        total_excel = 0 # The excel document's number of lines
 
-    f = open('bh5nd64t56A2127B.txt', 'r')
-    num_lines = sum(1 for line in open('bh5nd64t56A2127B.txt'))
+        folderName = folderName # folder containing .RDY files
+        fileName = folderName + '/' + fileName # .RDY file name
+        f = open(filaName, 'r')
 
-    linelist = f.readlines()
+        e = open(excelFile, 'r')
+        total_excel = len(e.readlines())
+        num_lines = sum(1 for line in open(fileName))
 
-    first = 9999
-    for i in range(len(linelist)):
-        if  (re.search(r"E\+\d\d\n|E\-\d\d\n", linelist[i]) or str(linelist[i]) == "Not Measured!\n" or str(linelist[i]) == "passed\n" or str(linelist[i])[0] == "-"):
-            count+=1
-            if i < first:
-                first = i
-                print(first)
+        linelist = f.readlines()
 
-    f.close()
-    
-    needed = num_lines - first
-    total = needed / 6
-    print(total)
-    return total    
-    
-  
+        first = 9999
+        for i in range(len(linelist)):
+            if  (re.search(r"E\+\d\d\n|E\-\d\d\n", linelist[i]) or str(linelist[i]) == "Not Measured!\n" or str(linelist[i]) == "passed\n" or str(linelist[i])[0] == "-"):
+                count+=1
+                if i < first:
+                    first = i
+                    print(first)
 
+        f.close()
+        
+        needed = num_lines - first
+        total = needed / 6
+        print(total)
 
-parser()
+        if (total < total_excel):
+            # Call the tkinter function to replace excelFile
+            replaceExcelFile(folderName, fileName)
+            
+            
+        # Call the % output part of the program           
+        return tally(folderName, fileName, excelFile) 
+        
+
+    parser()
 
 
