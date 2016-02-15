6a882cc2b9a80849f86fc5c7e30c0167bb244cdb
aaron-monahan <monahanam@g.cofc.edu>
2016-02-15 17:48:33 -0500

    syntax updates

--- TSD/TSD.py
+++ TSD/TSD.py
@@ -4,39 +4,39 @@ import TSD_Graphics
 class TSD:
 
     def main():
-    	done = 0
-    	wait_count = 0
+        done = 0
+        wait_count = 0
 
-    	# Predifined folder location
-    	folderName = ""
+        # Predifined folder location
+        folderName = ""
 
-    	# Predifined .rdy file name, eventually this will call a method in the parser program that monitors
-    	# the contents of the folderName
-    	fileName = ""
+        # Predifined .rdy file name, eventually this will call a method in the parser program
+        # that monitors the contents of the folderName
+        fileName = ""
 
-    	# Predifined .rdy file name
-    	excelFile = ""
+        # Predifined .rdy file name
+        excelFile = ""
 
-    	while(done != 1):
+        while(done != 1):
 
-    		if (wait_count > 5):
-    			done = 1
-    		else :
-				fileName = folder_monitor(folderName)
+            if (wait_count > 5):
+                done = 1
+            else :
+                fileName = folder_monitor(folderName)
 
-				if (fileName != ""):
-					parser(folderName, fileName, excelFile)
+                if (fileName != ""):
+                    parser(folderName, fileName, excelFile)
 
-				else:
-					wait()
-					wait_count += 1
+                else:
+                    wait()
+                    wait_count += 1
 
         return
 
 
      # Method that keeps track of the time, when 17 seconds has passed then return
-     def wait():
+    def wait():
 
-     	return
+        return
 
     main()
--- TSD/TSD_Graphics.py
+++ TSD/TSD_Graphics.py
@@ -16,21 +16,21 @@ class TSD_Graphics:
     # Method the recalculates the total percentage
     def calc_percent(machine_name, pass_type, current_machine_total_ones, current_machine_total_twos):
 
-    	if (pass_type == 1) :
-    		total_ones = current_machine_total_ones + 1
+        if (pass_type == 1) :
+                total_ones = current_machine_total_ones + 1
 
-    	else :
-    		total_twos = current_machine_total_twos + 1
+        else :
+                total_twos = current_machine_total_twos + 1
 
-    	percentage = total_twos / total_ones
+        percentage = total_twos / total_ones
 
-    	if (total_ones > 5):
-    		UI(machine_name, total_ones)
+        if (total_ones > 5):
+                UI(machine_name, total_ones)
 
-    	else if (total_twos > 5):
-    		UI(machine_name, total_twos)
+        elif (total_twos > 5):
+                UI(machine_name, total_twos)
 
-    	UI(machine_name, percentage)
+        UI(machine_name, percentage)
         return total_ones, total_twos
 
 
