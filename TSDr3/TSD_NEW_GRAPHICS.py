abf733e68c7213e6e926df620ab4bea088b2a049
John Parreno <johnparreno@rocketmail.com>
2016-04-07 14:50:22 -0400

    Updated new graphics

--- TSDr3/TSD_NEW_GRAPHICS.py
+++ TSDr3/TSD_NEW_GRAPHICS.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from time import sleep
+from win32api import GetSystemMetrics
+import time
 
 class ResizingCanvas(Canvas):
     def __init__(self,parent,**kwargs):
@@ -18,37 +19,82 @@ class ResizingCanvas(Canvas):
         #self.config(width=self.width, height=self.height)
         # rescale all the objects tagged with the "all" tag
         self.scale("all",0,0,wscale,wscale)
+        
+    def create_circle(self, x, y, r, **kwargs):
+        return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
 
-class wind:
-    def __init__(self):
-        self.root = Tk()
+class TSD_Graphics:
+
+    def __init__(self,root,canvas,machine,lastPass,x,y):
         #topFrame = Frame(root)
 
-        self.machineName = StringVar()
-        self.machineName.set('Test')
-        self.theLabel = Label(self.root,textvariable =self.machineName )
-        self.theLabel.pack(side = BOTTOM)
+##        self.machineName = StringVar()
+##        self.machineName.set('Test')
+##        self.theLabel = Label(self.root,textvariable =self.machineName )
+##        self.theLabel.pack(side = BOTTOM)
+##        
+##        self.canvas1 = ResizingCanvas(self.root,width = 1000, height = 500, bg = 'light grey')
+##        self.canvas1.pack(side = LEFT,expand=YES, fill=BOTH)
+##        self.circle1 = self.canvas1.create_oval(700,50,450,300,fill = 'red')
+##        self.circle2 = self.canvas1.create_oval(400,50,50,400,fill = 'red')
+##        
+##        
+##        self.root.update()
+        self.root = root
+        self.canvas = canvas
+        self.machine = machine
+        self.x = x
+        self.y = y
+        self.val = 0
+        self.count = 1
+        self.totalPass = 0
+        self.totalFail = 0
+        self.start = time.time()
+
+ 
         
-        self.canvas1 = ResizingCanvas(self.root,width = 1000, height = 500, bg = 'light grey')
-        self.canvas1.pack(side = LEFT,expand=YES, fill=BOTH)
-        self.circle1 = self.canvas1.create_oval(700,50,450,300,fill = 'red')
-        self.circle2 = self.canvas1.create_oval(400,50,50,400,fill = 'red')
-        
-        
-        self.root.update()
+        self.valArr = []
+        for i in range(0,199): ##initialize array holding values past 100 values (queue-like structure)
+            self.valArr.append(0)
+            
+        self.barArr = [] ##array containing rectangle objects
+        for i in range(0,199):
+            self.barArr.append(self.canvas.create_line(i*2+self.x,self.y,(i+1)*2+self.x,self.val+self.y)) ##initialize array containing rectangle objects
+
+        self.circle = self.canvas.create_circle(155+self.x,200+self.y,150,fill = 'white')
+
+        self.currentRuns = self.canvas.create_text(155+self.x,190 +self.y,text = "Initializing...",font = ("system",int(GetSystemMetrics(0)/40)))
 
     def update(self,text):
         self.machineName.set('CHANGED IT')
 
 
+def getColor(var):
+    if (Debug == 1):
+        print("Set colors")
+        
+    if var < THRESHOLD_FAIL_PERCENTAGE:
+        color = 'red'
+    elif var < THRESHOLD_WARNING_PERCENTAGE:
+        color = 'yellow'
+    else:
+        color = 'green'
+    return color
+
+root = Tk()
+canvas = ResizingCanvas(root,width = 1000, height = 560, bg = 'light grey')
+canvas.pack(side = LEFT,expand=YES, fill=BOTH)
+
+tsd = TSD_Graphics(root,canvas,'machine1','hello',0,0)
+tsd = TSD_Graphics(root,canvas,'machine1','hello',500,0)
+
+
+
 
     
     
-        
-test = wind()
-sleep(1)
-test.update('d')
+
 
 
 
