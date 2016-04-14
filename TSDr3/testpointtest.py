2a7b576cacd90e9aadb9fa8162f84c5b5ed00cef
John Parreno <johnparreno@rocketmail.com>
2016-04-14 09:30:53 -0400

    Update

--- TSDr3/testpointtest.py
+++
@@ -1,7 +0,0 @@
-def getTestPoints(self):
-        file_path = open(self.rdyFile, 'r')
-        linelist = file_path.readlines()
-        for i in range(43,2551,6):
-            print(linelist[i])
-        
-        file_path.close()
